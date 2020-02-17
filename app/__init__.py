3e2723964502c4206e48455f6fd55cc29c66ffb1
Ubuntu <ubuntu@ip-172-31-94-227.ec2.internal>
2020-02-17 00:31:32 +0000

    created flask email

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,11 @@ from flask_mail import Mail
 
 app = Flask(__name__) 
 
-app.config['SECRET_KEY'] = 'cheesecake' 
+app.config['SECRET_KEY'] = 'enter some pass key' 
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io' 
 app.config['MAIL_PORT'] = '2525' 
-app.config['MAIL_USERNAME'] = '918a19d90c585f' 
-app.config['MAIL_PASSWORD'] = '40f5063dbc9947' 
+app.config['MAIL_USERNAME'] = 'enter username' 
+app.config['MAIL_PASSWORD'] = 'enter password' 
 
 mail= Mail(app) 
 from app import views  
